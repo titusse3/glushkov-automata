cb949f6231f8f06b426687c1d43808719f7386d1
renauth6 <theo.renauxv@gmail.com>
2024-04-30 12:25:18 +0200

    change

--- app/Main.hs
+++ app/Main.hs
@@ -2,32 +2,24 @@ module Main
   ( main
   ) where
 
-import           Control.Monad                     (join)
-import           Exp                               (alphabet, firstE, followE,
-                                                    glushkov, lastE,
-                                                    linearisation)
-import qualified ExpHp                             as H
-import qualified ExpLex                            as A
-import           NFA                               (automatonToDot)
-
-import qualified Data.Set                          as Set
-
-import           Data.Maybe                        (fromJust)
-
-import           Data.Graph.Inductive.Graph
-import           Data.Graph.Inductive.PatriciaTree
-import           Data.GraphViz
-import           Data.GraphViz.Attributes.Complete
-import           Data.Text.Internal.Lazy           as LTIO
-import qualified Data.Text.Lazy                    as L
-import qualified Data.Text.Lazy.IO                 as TIO
+import           Data.GraphViz     (printDotGraph)
+import           Data.Maybe        (fromJust)
+import qualified Data.Text.Lazy.IO as TIO
+import           Exp               (glushkov)
+import qualified ExpHp             as H
+import qualified ExpLex            as A
+import           NFA               (accept, automatonToDot, automataToGraph)
+import Data.Graph.Inductive.Query.DFS
 
 main :: IO ()
 main = do
   -- let t = "(a+b).a*.b*.(a+b)*"
-  let t = "(a+b).a*.b*"
+  let t = "(a+b)*"
   -- t <- TIO.getLine
   let expT = fromJust $ H.parseExp (A.alexScanTokens t)
   let gluskov = glushkov expT
-  let c = automatonToDot gluskov
-  TIO.putStr $ printDotGraph c
+  TIO.putStr $ printDotGraph $ automatonToDot gluskov
+  print $ accept gluskov "ab"
+  let sccs = scc $ automataToGraph gluskov
+  putStrLn $ "// Composantes fortement connexes : "
+  mapM_ print sccs
--- src/NFA.hs
+++ src/NFA.hs
@@ -4,16 +4,18 @@ module NFA
   , isFinal
   , isStart
   , automatonToDot
+  , accept
   ) where
 
 import           Data.Graph.Inductive
+import Data.Graph.Inductive.Query.DFS
+
+import           Data.GraphViz                     
+import           Data.GraphViz.Attributes.Complete
 import           Data.Maybe                        (fromMaybe)
 import qualified Data.Set                          as Set
 import qualified Data.Text                         as T
 import qualified Data.Text.Lazy                    as LT
-
-import           Data.GraphViz
-import           Data.GraphViz.Attributes.Complete
 
 data NFA state transition = NFA
   { sigma   :: Set.Set transition
@@ -29,18 +31,18 @@ isFinal a = flip Set.member (final a)
 isStart :: Ord state => NFA state transition -> state -> Bool
 isStart a = flip Set.member (premier a)
 
-accept :: Ord state => NFA state transition  -> [transition] -> Bool
-accept a = accept'  i
-  where d = delta a
-        i = premier a
-        f = final a
-
+accept :: Ord state => NFA state transition -> [transition] -> Bool
+accept a = accept' i
+  where
+    d = delta a
+    i = premier a
+    f = final a
         -- accept' :: Set.Set state -> [transition] -> Bool
-        accept' s [] = not $ Set.null $ Set.intersection s f
-        accept' s (t:ts) = if Set.null s then 
-          False 
-          else 
-            accept' (Set.foldl (\s' x -> Set.union s' $ d x t) Set.empty s) ts
+    accept' s [] = not $ Set.null $ Set.intersection s f
+    accept' s (t:ts) =
+      if Set.null s
+        then False
+        else accept' (Set.foldl (\s' x -> Set.union s' $ d x t) Set.empty s) ts
 
 automataToGraph ::
      (Ord state, Show state, Show transition)
@@ -74,6 +76,7 @@ automatonToDot a = graphToDot params graph
               [ Shape $ shapeOf node
               , FillColor [toWColor $ colorOf node]
               , Style [SItem Filled []]
+              , ExtraAttrs [("style", if isComponentNode node then "rounded,filled" else "filled")]
               ]
         , fmtEdge = \(_, _, l) -> [Label $ StrLabel $ LT.fromStrict l]
         }
@@ -83,3 +86,4 @@ automatonToDot a = graphToDot params graph
     colorOf (val, _)
       | isStart a val = Green
       | otherwise = White
+    isComponentNode node = any (elem node) scc
